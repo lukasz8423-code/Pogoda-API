d54a015329a982a67a81837a9811aa74e3377e57
lukasz8423-code <lukasz8423@gmail.com>
2026-08-28 12:56:07 +0000

    Deploying to gh-pages from @ lukasz8423-code/Pogoda-API@134a53187d826047f336aa576b96764321b5ea86 🚀

--- android/app/src/main/java/com/pogodaapi/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.pogodaapi.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_OPENMETEO_API_KEY?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
